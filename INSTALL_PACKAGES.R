6ff5ce62518496ca90b535a8c0a14ba096a5d672
1carvercoleman <58525599+1carvercoleman@users.noreply.github.com>
2020-01-31 12:12:28 -0700

    Add files via upload

---
+++ INSTALL_PACKAGES.R
@@ -0,0 +1,8 @@
+# Run file to install dependencies
+install.packages("shiny", dependencies = TRUE)
+install.packages("imager", dependencies = TRUE)
+install.packages("MASS", dependencies = TRUE)
+install.packages("dequer", dependencies = TRUE)
+install.packages("magick", dependencies = TRUE)
+install.packages("tesseract", dependencies = TRUE)
+install.packages("DT", dependencies = TRUE)
