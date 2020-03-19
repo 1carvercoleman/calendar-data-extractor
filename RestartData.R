3c56a8647a49c25224ac0671143d78c2ed0e06f7
Carver Coleman <58525599+1carvercoleman@users.noreply.github.com>
2020-03-19 14:25:43 -0600

    Add files via upload

--- RestartData.R
+++ RestartData.R
@@ -1,5 +1,5 @@
 RestartData <- function() {
   final_data <- setNames(data.frame(matrix(ncol = NUMBER_OF_COLUMNS_IN_FINAL_DATA_FRAME, nrow = 0))
-                         , c("State", "County", "Year", "Type", "Month", "Day", "Item", "Vegetarian", "Sodium"))
+                         , c("State", "County", "Year", "Type", "Month", "Day", "Item", "Vegetarian", "Sodium", "Calories"))
   return(final_data)
 }
